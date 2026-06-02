22df86a0afffd4020732f92e6987c0c2ea57085b
Hung1 <Hungle4044@gmail.com>
2026-06-02 09:21:16 +0700

    .env in assets

--- lib/core/gen/assets.gen.dart
+++ lib/core/gen/assets.gen.dart
@@ -110,9 +110,13 @@ class $AssetsImagesGen {
 class Assets {
   const Assets._();
 
+  static const String aEnv = '.env';
   static const $AssetsColorsGen colors = $AssetsColorsGen();
   static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
+
+  /// List of all assets
+  static List<String> get values => [aEnv];
 }
 
 class AssetGenImage {
